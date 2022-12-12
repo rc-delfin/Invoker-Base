3bd9e09891c91e672f1123bac59147a1b233ee22
rcdelfin <irri.org>
2022-12-12 16:14:36 +0800

    added run lambda functions and flash messages

---
+++ scripts/invoke_lambda.py
@@ -0,0 +1,38 @@
+def run_lambda1():
+    print("Lambda 1 ran")
+
+
+def run_lambda2():
+    print("Lambda 2 ran")
+
+
+def run_lambda3():
+    print("Lambda 3 ran")
+
+
+def run_lambda4():
+    print("Lambda 4 ran")
+
+
+def run_lambda5():
+    print("Lambda 5 ran")
+
+
+def run_lambda6():
+    print("Lambda 6 ran")
+
+
+def run_lambda7():
+    print("Lambda 7 ran")
+
+
+def run_lambda8():
+    print("Lambda 8 ran")
+
+
+def run_lambda9():
+    print("Lambda 9 ran")
+
+
+def run_lambda0():
+    print("Lambda 0 ran")
