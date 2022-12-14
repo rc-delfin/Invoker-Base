f8a4b2f500dee21090a54300ce2c199b15945688
rcdelfin <irri.org>
2022-12-14 16:32:33 +0800

    applied DRY principle on invoke_lambda.py

--- scripts/invoke_lambda.py
+++ scripts/invoke_lambda.py
@@ -16,34 +16,7 @@ def success_fail(rn, value):
     else:
         flash("Option " + value + " selected. Lambda invocation failed")
 
+
 def run_lambda(value):
     rn = get_rand()
-    if value == "b1":
-        print("Lambda 1 ran")
-        success_fail(rn, value)
-    elif value == "b2":
-        print("Lambda 2 ran")
-        success_fail(rn, value)
-    elif value == "b3":
-        print("Lambda 3 ran")
-        success_fail(rn, value)
-    elif value == "b4":
-        print("Lambda 4 ran")
-        success_fail(rn, value)
-    elif value == "b5":
-        print("Lambda 5 ran")
-        success_fail(rn, value)
-    elif value == "b6":
-        print("Lambda 6 ran")
-        success_fail(rn, value)
-    elif value == "b7":
-        print("Lambda 7 ran")
-        success_fail(rn, value)
-    elif value == "b8":
-        print("Lambda 8 ran")
-        success_fail(rn, value)
-    elif value == "b9":
-        print("Lambda 9 ran")
-        success_fail(rn, value)
-    else:
-        pass
+    success_fail(rn, value)
